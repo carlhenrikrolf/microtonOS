41d57ea6eb026159037c66de0ac571ea87389819
carlhenrikrolf <henrik.aaslund@gmail.com>
2024-08-27 09:55:48 +0200

    restart is working

--- restart_engines.sh
+++ restart_engines.sh
@@ -1,16 +1,15 @@
 #! /usr/bin/bash
 
-systemctl stop pianoteq.service
-#pgrep -f '/home/pi/Pianoteq 8 STAGE/arm-64bit/Pianoteq 8 STAGE' | xargs kill
-#sleep 1
-sudo -u 'pi' '/home/pi/Pianoteq 8 STAGE/arm-64bit/Pianoteq 8 STAGE' --headless &
 systemctl stop tuneBfree_wrapper.service
-pgrep -f '/home/pi/microtonOS/tuneBfree_wrapper.py' | xargs kill
-#sleep 1
+pgrep -f '/home/pi/microtonOS/tuneBfree_wrapper.py' | xargs --no-run-if-empty kill
 /home/pi/microtonOS/tuneBfree_wrapper.py &
-pgrep -f '/home/pi/microtonOS/surge_xt_wrapper.py' | xargs kill
+pgrep -f '/home/pi/microtonOS/surge_xt_wrapper.py' | xargs --no-run-if-empty kill
 systemctl stop surge_xt_wrapper.service
-#sleep 1
 /home/pi/microtonOS/surge_xt_wrapper.py &
+systemctl stop pianoteq.service
+pgrep -f '/home/pi/microtonOS/start_pianoteq.sh' | xargs --no-run-if-empty kill
+pgrep -f '/home/pi/Pianoteq 8 STAGE/arm-64bit/Pianoteq 8 STAGE' | xargs --no-run-if-empty kill
+sudo --user 'pi' '/home/pi/microtonOS/start_pianoteq.sh' --headless &
+
 echo 'Engines restarted'
 
