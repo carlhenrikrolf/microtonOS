078d4106de72779b8d0f0495aa23597579e5c7ff
carlhenrikrolf <henrik.aaslund@gmail.com>
2024-08-27 09:03:18 +0200

    fixed restart engines such that it doesnt make lllots of duplicate processes. there is still something strange about restarting pianoteq

--- restart_engines.sh
+++ restart_engines.sh
@@ -1,9 +1,16 @@
 #! /usr/bin/bash
 
 systemctl stop pianoteq.service
-'/home/pi/Pianoteq 8 STAGE/arm-64bit/Pianoteq 8 STAGE' --headless &
+#pgrep -f '/home/pi/Pianoteq 8 STAGE/arm-64bit/Pianoteq 8 STAGE' | xargs kill
+#sleep 1
+sudo -u 'pi' '/home/pi/Pianoteq 8 STAGE/arm-64bit/Pianoteq 8 STAGE' --headless &
 systemctl stop tuneBfree_wrapper.service
+pgrep -f '/home/pi/microtonOS/tuneBfree_wrapper.py' | xargs kill
+#sleep 1
 /home/pi/microtonOS/tuneBfree_wrapper.py &
+pgrep -f '/home/pi/microtonOS/surge_xt_wrapper.py' | xargs kill
 systemctl stop surge_xt_wrapper.service
+#sleep 1
 /home/pi/microtonOS/surge_xt_wrapper.py &
+echo 'Engines restarted'
 
