18a012cc071209dc562a61a52148e980d41b96ad
carlhenrikrolf <henrik.aaslund@gmail.com>
2024-09-27 16:19:16 +0200

    firmware and encoders

--- src/mtsesp_master/encoders.py
+++ src/mtsesp_master/encoders.py
@@ -17,19 +17,44 @@ class CustomLayout(BaseCustom):
 
 class Encoders
 
-	def __init__(self, outport):
+	def __init__(self,
+		outport,
+		init_equave=0,
+		equave_range=range(-2,3),
+		init_tuning_program = ...,
+		tuning_presets=24,
+		init_layout_program = ...,
+		layout_presets=4,
+		init_transposition=[69]*4,
+		transposition_range=[range(...)]*4,
+		init_dilation=[3]*4,
+		dilation_range=[range(0,8)]*4,
+		):
+			
+		assert layout_presets == len(init_transposition) == len(transposition_range) == len(init_dilation) == len(dilation_range)
+		
 		self.outport = outport
+		self.init_equave = init_equave
+		self.equave_range = equave_range
+		self.init_tuning_program = init_tuning_program
+		self.tuning_presets = tuning_presets
+		self.init_layout_program = init_layout_program
+		self.layout_presets = layout_presets
+		self.init_transposition = init_transposition
+		self.transposition_range = transposition_range
+		self.init_dilation = init_dilation
+		self.dilation_range = dilation_range
+		
+		self.equave = init_equave
+		self.tuning_program = init_tuning_program
+		self.layout_program = init_layout_program
+		self.transposition = self.init_transposition
+		self.dilation = init_dilation
+		
 		self.custom_tuning = CustomTuning(self.outport)
 		self.custom_layout = CustomLayout(self.outport)
 		self.is_left_right = False
 		self.is_up_down = False
-		self.equave = 0
-		self.equaves = range(-2, 3) # shouldnt this go in main file?
-		self.transposition = 69
-		self.dilation = 3
-		self.is_menu = False
-		self.tuning_program = ...
-		self.layout_program = 0
 		
 	def handle(self, msg):
 		if xq.is_sysex(msg):
@@ -43,10 +68,24 @@ class Encoders
 			is_altered += self.flip_up_down(msg)
 			...
 			if is_altered:
-				self.send()
-			
-	def send(self):
-		...
+				return {'equave': self.equave,
+				'tuning_program': self.tuning_program,
+				'layout_program': self.layout_program,
+				'transposition': self.transposition[self.layout_program],
+				'dilation': self.dilation[self.layout_program],
+				'is_left_right': self.is_left_right,
+				'is_up_down': self.is_up_down,
+				'is_editing': ...,
+				'is_custom_tuning': ...,
+				'custom_steps': ...,
+				'custom_numerator':, ...,
+				'custom_denominator': ...,
+				'is_custom_layout': ...,
+				'custom_up': ...,
+				'custom_left': ...,
+				'custom_splits': ...,
+				}
+			return None
 			
 		
 	def is_on(self):
@@ -55,24 +94,41 @@ class Encoders
 	
 	def equave_up(self, msg):
 		if xq.is_sysex(msg, [xq.click, xq.octave_up, xq.is_pressed]) and self.is_on():
-			pass # move equave up
-			# move entire tuning system
+			self.equave += 1 if self.equave+1 in self.equave_range else 0
+			if self.equave == self.init_equave:
+				xq.send(outport, xq.sysex(xq.color, xq.octave_up, xq.green))
+			elif self.equave == max(self.equave_range):
+				xq.send(outport, xq.sysex(xq.color, xq.octave_up, xq.white))
+			else:
+				xq.send(outport, xq.sysex(xq.color, xq.octave_up, xq.dark)) # update blank
 			return True
 		return False
 	
 	def equave_down(self, msg):
 		if xq.is_sysex(msg, [xq.click, xq.octave_down, xq.is_pressed]) and self.is_on():
-			pass # move equave down
-			# move entire tuning system
+			self.equave -= 1 if self.equave-1 in self.equave_range else 0
+			if self.equave == self.init_equave:
+				xq.send(outport, xq.sysex(xq.color, xq.octave_down, xq.green))
+			elif self.equave == min(self.equave_range):
+				xq.send(outport, xq.sysex(xq.color, xq.octave_down, xq.white))
+			else:
+				xq.send(outport, xq.sysex(xq.color, xq.octave_down, xq.dark)) # update blank
 			return True
 		return False
 		
 	def flip_left_right(self, msg):
 		if xq.is_sysex(msg, [xq.click, xq.left_right, xq.is_pressed]) and self.is_on():
-			pass 
+			self.is_left_right = not self.is_left_right
+			# send color
+			return True
+		return False
 		
 	def flip_up_down(self, msg):
-		if xq.is_sysex(msg, [xq.click, xq.left_right
+		if xq.is_sysex(msg, [xq.click, xq.up_down, xq.is_pressed]) and self.is_on():
+			self.is_up_down = not self.is_up_down
+			# send color
+			return True
+		return False
 		
 	def transpose(self, msg):
 		...
