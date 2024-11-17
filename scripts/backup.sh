62103668e7b29830a9d042bf9e3a779627582736
carlhenrikrolf <henrik.aaslund@gmail.com>
2024-11-17 16:51:18 +0100

    surge presets

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,3 +2,4 @@
 
 cp -rf /home/pi/.ladish /home/pi/microtonOS/config/
 cp -rf /home/pi/.local/share/Modartt/Pianoteq /home/pi/microtonOS/config/
+cp -rf '/home/pi/Documents/Surge XT' /home/pi/microtonOS/config/
--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -3,6 +3,6 @@ init_engine = 'Pianoteq'
 engine_banks_pgms = [
 	['Pianoteq', (9,4,4,2)],
 	['tuneBfree', (17,17,17)],
-	['Surge XT', (4,4,4)],
+	['Surge XT', (4,7,2,2)],
 	['WiDi', (17,17,17)],
 ]
