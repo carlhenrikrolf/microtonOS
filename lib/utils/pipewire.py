2f419017d6e3cd0634b04b4d2b6e58dcd01cadc5
carlhenrikrolf <henrik.aaslund@gmail.com>
2025-09-04 09:23:32 +0200

    pipewire4all

--- lib/utils/pipewire.py
+++ lib/utils/pipewire.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 
 
 def get_volume(port="@DEFAULT_AUDIO_SINK@"):
@@ -21,5 +22,45 @@ def set_volume(level=None, muted=None, port="@DEFAULT_AUDIO_SINK@"):
         subprocess.run(["wpctl", "set-mute", port, "1" if muted else "0"])
 
 
-def set_gain(level=None, muted=None):
-    set_volume(level, muted, port="@DEFAULT_AUDIO_SOURCE@")
+def set_volume4all(level=None, muted=None):
+    status = subprocess.check_output(["wpctl", "status"])
+    status = status.decode()
+    # find the integer ids of all audio sinks
+    sinks_section = re.search(
+        r"Sinks:\n(.*?)(?=\n\s*(?:├─|└─)?\s*\w+ endpoints:|\n\s*├─ Sources:|$)",
+        status,
+        re.DOTALL,
+    )
+    ports = []
+    if sinks_section:
+        # Regex to find lines starting with an optional tree character, optional '*',
+        # whitespace, and then capturing the numerical ID.
+        ports = re.findall(
+            r"^\s*(?:│|\s)?\s*(?:\*\s*)?(\d+)\.", sinks_section.group(1), re.MULTILINE
+        )
+    print(ports)
+    for port in ports:
+        set_volume(level, muted, port=port)
+
+
+def set_gain4all(level=None, muted=None):
+    status = subprocess.check_output(["wpctl", "status"])
+    status = status.decode()
+    # find the integer ids of all audio sources
+    sources_section = re.search(
+        r"Sources:\n(.*?)(?=\n\s*(?:├─|└─)?\s*\w+ endpoints:|$)", status, re.DOTALL
+    )
+    ports = []
+    if sources_section:
+        # Regex to find lines starting with an optional tree character, optional '*',
+        # whitespace, and then capturing the numerical ID.
+        ports = re.findall(
+            r"^\s*(?:│|\s)?\s*(?:\*\s*)?(\d+)\.", sources_section.group(1), re.MULTILINE
+        )
+    print(ports)
+    for port in ports:
+        set_gain(level, muted, port=port)
+
+
+def set_gain(level=None, muted=None, port="@DEFAULT_AUDIO_SOURCE@"):
+    set_volume(level, muted, port=port)
