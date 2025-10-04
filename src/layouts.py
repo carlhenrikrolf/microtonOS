f879e1ce0571b099fb4e43a34a0d35e4252fb484
carlhenrikrolf <henrik.aaslund@gmail.com>
2025-10-04 11:21:09 +0200

    sequencer added. moving of isomorphic layout started

--- src/layouts.py
+++ src/layouts.py
@@ -1,18 +1,259 @@
 import numpy as np
 from midi_implementation.midi1 import percussion as perc
 from midi_implementation.intuitive_instruments import exquis2_1_0 as xq
-from utils import load_config
 
-config = {"general_settings": load_config(__file__, "../config/general_settings.toml")}
-red = np.array(config["general_settings"]["palette"]["red"])
-magenta = np.array(config["general_settings"]["palette"]["magenta"])
+red = np.array([1, 0, 0])
+magenta = np.array([1, 0, 1])
 
 
-class TwelveTone:
+def hexagonal(
+    height,
+    width,
+    up,
+    right,
+    bottom_crop=False,
+    top_right=69,
+    bottom_right=None,
+    bottom_left=None,
+    top_left=None,
+):
+    layout = np.zeros([height, width], int)
+    for row in range(height):
+        for col in range(width):
+            term = right * ((height - row + int(bottom_crop)) // 2)
+            layout[row, col] = up * (height - row) + term + right * col
+    if top_left is not None:
+        diff = top_left - layout[0, 0]
+    elif bottom_left is not None:
+        diff = bottom_left - layout[-1, 0]
+    elif bottom_right is not None:
+        diff = bottom_right - layout[-1, -1]
+    else:
+        diff = top_right - layout[0, -1]
+    layout += diff
+    return layout
+
+
+def rectangular(
+    height,
+    width,
+    up,
+    right,
+    bottom_crop=None,
+    top_right=69,
+    bottom_right=None,
+    bottom_left=None,
+    top_left=None,
+):
+    layout = np.zeros([height, width], int)
+    for row in range(height):
+        for col in range(width):
+            layout[row, col] = up * (height - row) + right * col
+    if top_left is not None:
+        diff = top_left - layout[0, 0]
+    elif bottom_left is not None:
+        diff = bottom_left - layout[-1, 0]
+    elif bottom_right is not None:
+        diff = bottom_right - layout[-1, -1]
+    else:
+        diff = top_right - layout[0, -1]
+    layout += diff
+    return layout
+
+
+def dash(height, width):
+    """
+    Used for splitting the layout lengthwise.
+    Produces a list of coordinates forming a straight line.
+    """
+    separator = []
+    for col in range(width):
+        separator.append((round(height / 2) - 1, col))
+    return separator
+
+
+def backslash(height, width):
+    separator = []
+    separator.append((round(height / 2) - 1, round(width / 2)))
+    while True:
+        x = separator[-1][1]  # up
+        y = separator[-1][0] - 1
+        if y not in range(height):
+            break
+        separator.append((y, x))
+        x = separator[-1][1] + 1  # right
+        if x not in range(width):
+            break
+        separator.append((y, x))
+        x = separator[-1][1] + 1  # right
+        if x not in range(width):
+            break
+        separator.append((y, x))
+        x = separator[-1][1] + 1  # up right
+        y = separator[-1][0] - 1
+        if x not in range(width) or y not in range(height):
+            break
+        separator.append((y, x))
+    while True:
+        x = separator[0][1] - 1  # down left
+        y = separator[0][0] + 1
+        if x not in range(width) or y not in range(height):
+            break
+        separator.insert(0, (y, x))
+        x = separator[0][1] - 1  # left
+        if x not in range(width):
+            break
+        separator.insert(0, (y, x))
+        x = separator[0][1] - 1  # left
+        if x not in range(width):
+            break
+        separator.insert(0, (y, x))
+        y = separator[0][0] + 1
+        if x not in range(width):
+            break
+        separator.insert(0, (y, x))
+    flipped_separator = [(y, width - 1 - x) for (y, x) in separator]
+    return flipped_separator
+
+
+def slash(height, width):
+    separator = backslash(height, width)
+    mid = round(height / 2) - 1
+    n = len(separator)
+    inverted = []
+    for i in range(n):
+        x = separator[i][1]
+        y = mid + (mid - separator[i][0])
+        inverted.append((y, x))
+    return inverted
+
+
+def endpoints(separator):  # potential add-on. add overlaps
+    xmin = min([i[1] for i in separator])
+    xmax = max([i[1] for i in separator])
+    ymax = max([i[0] for i in separator])
+    ymin = min([i[0] for i in separator])
+    lefts = []
+    rights = []
+    for i in separator:
+        if i[1] == xmin:
+            lefts.append(i)
+        if i[1] == xmax:
+            rights.append(i)
+    left = lefts[0]
+    for i in lefts:
+        if min(abs(i[0] - ymax), abs(i[0] - ymin)) > min(
+            abs(left[0] - ymax), abs(left[0] - ymin)
+        ):
+            left = i
+    right = rights[0]
+    for i in rights:
+        if min(abs(i[0] - ymax), abs(i[0] - ymin)) > min(
+            abs(right[0] - ymax), abs(right[0] - ymin)
+        ):
+            right = i
+    return left, right
+
+
+def split(height, width, up, right, grid, separation, kind, top_right=69, overlap=1):
+    """
+    Used to create a split layout.
+    grid is either rectangular or square from above
+    The separator can be a dash, a slash, or a backslash.
+    The kind is either 'parallel', meaning that
+    going past the left side on the lower takes you to right side of the higher.
+    Or, the kind is 'sequential', meaning that
+    going up the upper right corner on lower takes you to the lower left corner on higher.
+    """
+    layout = grid(height, width, up, right, top_right=top_right)
+    separator = separation(height, width)
+    left_end, right_end = endpoints(separator)
+    if kind == "parallel":
+        bottom_height = height - right_end[0] - 1
+        bottom = grid(bottom_height, width, up, right, top_right=layout[0, 0 + overlap])
+    elif kind == "sequential":
+        bottom_height = height - right_end[0]
+        bottom = grid(
+            bottom_height,
+            width,
+            up,
+            right,
+            top_right=layout[left_end[0], left_end[1] + overlap],
+        )
+    else:
+        raise Warning(
+            "kind must be either 'parallel' or 'sequential'. (If both, 'parallel takes precedence.)"
+        )
+    mid_height = height - bottom_height + 1
+    overlap_crop = False if bottom_height % 2 > 0 else True
+    mid = grid(
+        mid_height,
+        width,
+        up,
+        right,
+        bottom_crop=overlap_crop,
+        bottom_right=bottom[0, -1],
+    )
+    lower = np.concatenate([mid[:-1, :], bottom])
+    for y, x in separator:
+        for i in range(y + 1, height):
+            layout[i, x] = lower[i, x]
+    for i in separator:
+        layout[i] = -1
+    return layout
+
+
+def clean(layout):
+    clean_layout = layout.tolist()
+    for i, row in enumerate(clean_layout):
+        for j, note in enumerate(row):
+            if note not in range(0, 128):
+                clean_layout[i][j] = -1
+    return clean_layout
+
+
+def crop(layout):
+    n = len(layout)
+    for i, row in enumerate(layout):
+        if (n - i) % 2 == 0:
+            row.pop(-1)
+    return layout
+
+
+class Exquis:
+    def __init__(self):
+        pass
+
+    def translate(self):
+        pass
+
+    def flip(self):
+        pass
+
+    def dilate(self):
+        pass
+
+
+class HarmonicTable:
     pass
 
 
-class EqualTempered:
+def f3(d):
+    if d % 3 > 0:
+        out = round(d / 3)
+    else:
+        out = 0
+        for i in range(1, int(d / 3) + 1):
+            if i > out and i % 2 > 0:
+                out = i
+    return out
+
+
+class WickiHayden:
+    pass
+
+
+class Janko:
     pass
 
 
@@ -26,7 +267,7 @@ class Isomorphic:
     def kind(self, id=None):
         pass
 
-    def dilation(self, id=None):
+    def dilate(self, id=None):
         pass
 
     def manual(self, id=None):
---
+++ src/sequencer.py
@@ -0,0 +1,86 @@
+import mido
+import numpy as np
+
+n_notes = 128
+n_channels = 16
+per32 = 3
+
+
+class StepSequencer:
+    """
+    Note that this step sequencer is intended for drum sounds
+    and therefore does not transmit note off messages.
+    If the time signature is x/y, then set the dividend to x and the divisor to y.
+    """
+
+    def __init__(self, dividend=4, divisor=4, display_length=6):
+        self.divisor = divisor
+        self.display_length = display_length
+        self.counter = 0
+        self.step = 0
+        self.is_pressed = np.full(shape=(n_notes, n_channels), fill_value=0, dtype=int)
+        self.velocity = np.full(
+            shape=(n_notes, n_channels, dividend), fill_value=0, dtype=int
+        )
+
+    def time_signature(
+        self, dividend
+    ):  # changing the time signature resets the recording
+        self.step = 0
+        self.is_pressed = np.full(shape=(n_notes, n_channels), fill_value=0, dtype=int)
+        self.velocity = np.full(
+            shape=(n_notes, n_channels, dividend), fill_value=0, dtype=int
+        )
+
+    def divide_tempo(self, divisor):
+        self.divisor = divisor
+
+    def record(self, msg, dividend=None):
+        if hasattr(msg, "note"):
+            if msg.type == "note_on" and msg.velocity > 0:
+                self.is_pressed[msg.note, msg.channel] = 1
+                erase = (
+                    self.is_pressed.min() >= 0
+                    and np.count_nonzero(self.is_pressed) == 1
+                )
+                if erase:  # instead of overdubbing, erase the previous data
+                    self.velocity[:, :, self.step] = 0
+                self.velocity[msg.note, msg.channel, self.step] = msg.velocity
+            elif msg.type in ["note_on", "note_off"]:
+                self.is_pressed[msg.note, msg.channel] = -1
+            proceed = self.is_pressed.min() == -1 and self.is_pressed.max() <= 0
+            if proceed:  # advance the step sequencer when all notes have been released
+                self.is_pressed[:, :] = 0
+                dividend = self.velocity.shape[2]
+                self.step = (self.step + 1) % dividend
+
+    def play(self, msg):
+        self.step = 0  # reset for next recording
+        notes_on = []
+        if msg.type == "clock":
+            ticks_per_beat = (32 // self.divisor) * per32
+            on_beat = self.counter % ticks_per_beat == 0
+            if on_beat:
+                step = self.counter // ticks_per_beat
+                step %= self.velocity.shape[2]  # return to beat 0 after one measure
+                notes, channels = self.velocity[:, :, step].nonzero()
+                for note, channel in zip(notes, channels):
+                    note_on = mido.Message(
+                        "note_on",
+                        note=note,
+                        channel=channel,
+                        velocity=self.velocity[note, channel, step],
+                    )
+                    notes_on.append(note_on)
+            self.counter += 1
+        return notes_on
+
+    def display(self):
+        binary = np.zeros(shape=self.display_length, dtype=int)
+        for i in range(
+            self.display_length
+        ):  # create a binary representation of the current position
+            if self.divisor >= 32 / (2**i):
+                j = self.display_length - 1 - i
+                binary[j] = (self.counter // (2**i * per32)) % 2
+        return binary.tolist()
